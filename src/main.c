604c69491e5b65242dd341c73df5849d56790b95
LeaoMartelo <leaomartelo@gmail.com>
2024-10-24 23:52:53 -0300

    trolemba

--- src/main.c
+++ src/main.c
@@ -6,11 +6,26 @@ int main(void) {
 
     SetTargetFPS(60);
 
+    float screen_width = GetScreenWidth();
+    float screen_heigth = GetScreenHeight();
+
+    Rectangle quadrado = {
+        .width = 100,
+        .height = 100,
+    };
+
+    Vector2 quadrado_pos = {screen_width - quadrado.width / 2, screen_heigth - quadrado.height / 2};
+
+    quadrado.x = quadrado_pos.x;
+    quadrado.y = quadrado_pos.y;
+
     while (!WindowShouldClose()) {
 
         BeginDrawing();
         {
             ClearBackground(BLACK);
+
+            DrawRectangleRec(quadrado, RED);
         }
         EndDrawing();
     }
